414658a33bf1d4277c941186f2167197c27deb9e
SeptemberHX <tianjianshan@outlook.com>
2019-01-02 19:55:35 +0800

    Bug fix k8s_install.sh need a version parameter

--- k8s_install.sh
+++ k8s_install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [[ $# != 2 ]]; then 
+    echo "$0 k8s_version"
+    exit 0
+fi 
+
+K8S_VERSION=$1
+
 # turn off swap, firewall and selinux
 swapoff -a
 
@@ -14,7 +21,7 @@ systemctl enable docker
 systemctl start docker
 
 # add k8s repo and install it
-if [ ! -f "/etc/yum.repos.d/kubernetes.repo"]; then
+if [ ! -f "/etc/yum.repos.d/kubernetes.repo" ]; then
     cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
@@ -29,7 +36,7 @@ fi
 
 sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 
-yum install -y kubelet kubeadm kubectl etcd --disableexcludes=kubernetes
+yum install -y kubelet-$K8S_VERSION.x86_64 kubeadm-$K8S_VERSION.x86_64 kubectl-$K8S_VERSION.x86_64 etcd --disableexcludes=kubernetes
 
 systemctl enable kubelet && systemctl start kubelet
 cat <<EOF >  /etc/sysctl.d/k8s.conf
