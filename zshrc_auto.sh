dafcfba44a32e89f4039c59c64919fcf031fe4a2
SeptemberHX <tianjianshan@outlook.com>
2019-01-10 14:37:48 +0800

    add oh my zsh

--- zshrc_auto.sh
+++ zshrc_auto.sh
@@ -1,5 +1,10 @@
 #!/bin/bash 
 
+# oh_my_zsh
+if [[ ! -d "${HOME}/.oh-my-zsh" ]]; then 
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
 # syntax highlight
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${HOME}/.zsh/zsh-syntax-highlighting
 echo "# zsh-syntax-highlighting" >> ${HOME}/.zshrc
