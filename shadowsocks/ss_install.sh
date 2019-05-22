4f2b38cc440ab5e5fe91d351cdb2b09ff9d09782
SeptemberHX <tianjianshan@outlook.com>
2019-05-22 21:43:02 +0800

    add scripts for shadowsocks[shadowsocks, privoxy, proxychains4]

---
+++ shadowsocks/ss_install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo " =========>>> Install proxychains4 <<<========= "
+git clone https://github.com/rofl0r/proxychains-ng.git proxychains-ng
+cd proxychains-ng
+./configure
+make
+make install
+cp ./config/proxychains.config /etc
+
+echo " =========>>> Install privoxy <<<========= "
+apt install privoxy
+cp ./config/config /etc/privoxy/config
+
+echo " =========>>> Install shadowsocks <<<========== "
+pip install shadowsocks
+cp ./config/shadowsocks.service /etc/systemd/system
+systemctl enable shadowsocks
+
+echo "Put your shadowsocks.json to /etc/shadowsocks/shadowsocks.json"
+echo "Then execute: systemctl start shadowsocks"
