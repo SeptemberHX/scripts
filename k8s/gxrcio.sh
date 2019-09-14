49bfb1ac287193e65de958ce069e94989a059113
SeptemberHX <tianjianshan@outlook.com>
2019-09-14 11:00:09 +0800

    add gxrcio.sh

---
+++ k8s/gxrcio.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ $# != 1 ]]; then
+    echo "$0 image_name"
+    exit 0
+fi
+
+echo "------------------------"
+echo -e "\033[34mPull image [$image_name]\033[0m"
+raw_image_name=$image_name
+new_image_name=${image_name/k8s.gcr.io/gcrxio}  # replace k8s.gcr.io with gcrxio
+docker pull $new_image_name
+docker tag $new_image_name $raw_image_name
+docker rmi $new_image_name
+echo "------------------------"
