41bc7ad2f53f3dc34b1e453633db8df3d4c0a9eb
SeptemberHX <september_hx@outlook.com>
2019-11-20 16:18:38 +0800

    add k8s

--- basic_utils.sh
+++ basic_utils.sh
@@ -4,3 +4,8 @@
 
 yum -y install epel-release
 yum install -y net-tools git curl vim htop zsh byobu yum-versionlock iperf3
+
+git clone https://github.com/SeptemberHX/scripts.git
+cd scripts
+
+./k8s/k8s_install.sh 1.16.2-0
