7acdee8349c366bb84f0763a930ef7f84c70254a
SeptemberHX <tianjianshan@outlook.com>
2020-10-09 16:38:40 +0800

    change k8s

--- k8s/k8s_gxrcio.sh
+++ k8s/k8s_gxrcio.sh
@@ -13,7 +13,7 @@ do
     echo "------------------------"
     echo -e "\033[34mPull image [$image_name]\033[0m"
     raw_image_name=$image_name
-    new_image_name=${image_name/k8s.gcr.io/gcrxio}  # replace k8s.gcr.io with gcrxio
+    new_image_name=${image_name/k8s.gcr.io/registry.aliyuncs.com\/google_containers}  # replace k8s.gcr.io with gcrxio
     docker pull $new_image_name
     docker tag $new_image_name $raw_image_name
     docker rmi $new_image_name
