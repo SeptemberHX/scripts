2e9f2bc855c54a5862e76ddaa6712861e66844e2
SeptemberHX <september_hx@outlook.com>
2021-01-16 21:04:17 +0800

    add: python post server for debug

---
+++ lab/debug_post_server.py
@@ -0,0 +1,31 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from sys import argv
+
+
+class Request(BaseHTTPRequestHandler):
+    def handler(self):
+        print("data:", self.rfile.readline().decode())
+        self.wfile.write(self.rfile.readline())
+
+    def do_POST(self):
+        print(self.headers)
+        print(self.command)
+        req_datas = self.rfile.read(int(self.headers['content-length']))  # 重点在此步!
+        print(req_datas.decode())
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write('finished'.encode('utf-8'))
+
+
+if __name__ == '__main__':
+    port = 8080
+    try:
+        port = int(argv[0])
+    except Exception as e:
+        pass
+
+    host = ('localhost', port)
+    server = HTTPServer(host, Request)
+    print("Starting server, listen at: %s:%s" % host)
+    server.serve_forever()
