3beebf512d58e6fef309ac0f01150ad6e1061af6
SeptemberHX <september_hx@outlook.com>
2021-01-17 11:55:11 +0800

    add: colorful output!

--- lab/debug_post_server.py
+++ lab/debug_post_server.py
@@ -1,4 +1,5 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from colorama import Fore, Back, Style
 from sys import argv
 
 
@@ -8,14 +9,19 @@ class Request(BaseHTTPRequestHandler):
         self.wfile.write(self.rfile.readline())
 
     def do_POST(self):
+        print(f'{Back.GREEN}--------------------> New Post Request Starts <--------------------{Style.RESET_ALL}')
+        print(f'{Back.YELLOW}==========> Headers:{Style.RESET_ALL}')
         print(self.headers)
+        print(f'{Back.YELLOW}==========> Command:{Style.RESET_ALL}')
         print(self.command)
         req_datas = self.rfile.read(int(self.headers['content-length']))  # 重点在此步!
+        print(f'{Back.YELLOW}==========> Data:{Style.RESET_ALL}')
         print(req_datas.decode())
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
         self.wfile.write('finished'.encode('utf-8'))
+        print(f'{Back.BLUE}--------------------> New Post Request Ends <--------------------{Style.RESET_ALL}')
 
 
 if __name__ == '__main__':
